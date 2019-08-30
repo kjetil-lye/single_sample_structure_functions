ed94430b5be52d27602924a32dd2a45870d2208b
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-08-30 15:13:54 +0200

    added script to compute deviation from mean field

---
+++ python/compute_deviation_from_mean.py
@@ -0,0 +1,123 @@
+"""
+For each sample, computes the deviation from the mean field. That is, it computes
+
+   \hat{u}_k := u_k-\bar{u}
+"""
+import numpy as np
+import netCDF4
+import copy
+import collections
+
+def get_shape(input_filename):
+    with netCDF4.Dataset(input_filename) as f:
+        for variable in f.variables.keys():
+            if variable != 'time':
+                return copy.deepcopy(f.variables[variable].shape)
+
+def get_conserved_variable_name(variable):
+    return str(re.search(r'sample_\d+_(.+)', variable).group(1))
+    
+def compute_mean_field(input_filename):
+    mean_field_shape = get_shape(input_filename)
+    mean_per_conserved_variable = \
+        collections.defaultdict(lambda : np.zeros(mean_field_shape))
+
+    number_of_samples = collections.defaultdict(int)
+    with netCDF4.Dataset(input_filename) as f:
+        for variable in f.variables.keys():
+            if variable != 'time':
+                data = f.variables[variable][:,:,:]
+                
+                conserved_name = get_conserved_variable_name(variable)
+                
+                mean_per_conserved_variable[conserved_name] += data
+
+                number_of_samples[conserved_name] += 1
+
+    for conserved_variable in mean_per_conserved_variable.keys():
+        if number_of_samples[conserved_name] == 0:
+            raise Exception(f"No samples found for {conserved_variable}")
+        
+        mean_per_conserved_variable[conserved_variable] /= number_of_samples[conserved_name]
+
+
+    return mean_per_conserved_variable
+    
+    
+def get_conserved_variables(input_filename):
+    variables = set()
+    with netCDF4.Dataset(input_filename) as f:
+        for variable in f.variables.keys():
+            re_match = re.search(r'sample_\d+_(.+)', variable)
+            if re_match:
+                variables.add(re_match.group(1))
+    return variable
+
+
+def compute_deviation(*, input_filename, output_filename):
+    mean_field = compute_mean_field(input_filename)
+
+    with netCDF4.Dataset(input_filename) as inputfile:
+        with netCDF4.Dataset(output_filename, 'w', format='NETCDF4_CLASSIC') as outputfile:
+            
+            xdim = None
+            ydim = None
+            zdim = None
+
+            for variable in inputfile.variables.keys():
+                
+                if variable == 'time':
+                    time = inputfile.variables[variable][0]
+                    tdim = outf.createDimension('t', 1)
+                    t = outf.createVariable("time", np.float64, ("t",))
+                    t[0] = time
+                else:
+                    conserved_name = get_conserved_variable_name(variable)
+                    data = inputfile.variables[variable]
+
+                    if xdim is None:
+                        
+                        xdim = outf.createDimension("x", d.shape[0])
+                        ydim = outf.createDimension("y", d.shape[1])
+                        zdim = outf.createDimension("z", d.shape[2])
+
+                newvar = outf.createVariable(variable, d.dtype, ("x", "y", "z"))
+                newvar[:,:,:] = d[:,:,:] - mean_field[conserved_name]
+
+            outf.setncattr("WORKING_DIR", os.getcwd())
+            outf.setncattr("IMPORTANT_NODE", """
+This file was converted with the script python/compute_deviation_from_mean.py
+in the single_sample_structure_functions repository. This was done as a postprocessing step
+            """)
+
+
+            outf.setncattr("COMMAND_RUN_TO_COMPUTE_DEVIATION", " ".join([
+                    sys.executable,
+                    *sys.argv]))
+
+            with open(sys.argv[0]) as scriptfile:
+                outf.setncattr("SCRIPTFILE", scriptfile.read())
+
+
+            for attr_name, attr_value in attributes.items():
+                outf.setncattr(attr_name, attr_value)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    
+    parser = argparse.ArgumentParser(description='Compute the deviation from the mean')
+
+    parser.add_argument('--input', type=str, required=True,
+                    help='input filename')
+
+    parser.add_argument('--output', type=str, required=True,
+                    help='output filename')
+
+    
+    
+    args = parser.parse_args()
+
+    compute_deviation(input_filename=args.input,
+                      output_filename=args.output)
