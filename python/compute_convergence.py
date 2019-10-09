65b5aefc44e842aabb204633fcb921df461f38b0
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-10-09 17:39:46 +0200

    added convergence scripts

--- python/compute_convergence.py
+++ python/compute_convergence.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import netCDF4
 
-conserved_variables_stochastic = [
+conserved_variables_default = [
         "rho",
         "mx",
         "my",
@@ -13,12 +13,6 @@ conserved_variables_stochastic = [
         ]
 
 
-conserved_variables_deterministic = [
-        "sample_0_rho",
-        "sample_0_mx",
-        "sample_0_my",
-        "sample_0_E"
-        ]
 
 def stats_latex(name, resolution):
     inner_name = f'u^{{{resolution}}}'
@@ -31,33 +25,94 @@ def stats_latex(name, resolution):
     
     return inner_name
 
-
-def load_file(filename, conserved_variables):
-    
+def load_file_single_sample(filename, conserved_variables):
     with netCDF4.Dataset(filename) as f:
         for attr in f.ncattrs():
             plot_info.add_additional_plot_parameters(filename.replace("/", "_") + "_" + attr, f.getncattr(attr))
             
-        resolution = f.variables[conserved_variables[0]].shape[0]
+
+        resolution = f.variables[f'sample_0_{conserved_variables[0]}'].shape[0]
         data = np.zeros((resolution, resolution, len(conserved_variables)))
         for variable_index, variable in enumerate(conserved_variables):
-            data[:,:,variable_index] = f.variables[variable][:,:,0]
+            data[:,:,variable_index] = f.variables[f'sample_0_{variable}'][:,:,0]
             
         return data
+
+
+
+def load_file_mean(filename, conserved_variables):
+    with netCDF4.Dataset(filename) as f:
+        for attr in f.ncattrs():
+            plot_info.add_additional_plot_parameters(filename.replace("/", "_") + "_" + attr, f.getncattr(attr))
+        
+        resolution = f.variables[conserved_variables[0]].shape[0]
+        mean = np.zeros((resolution, resolution, len(conserved_variables)))
+        
+        for sample in range(resolution):
+            for variable_index, variable in enumerate(conserved_variables):
+                key = f'sample_{sample}_{variable}'
+                mean[:,:,variable_index] += f.variables[key][:,:,0]
+        
+        mean /= resolution
+        
+        return mean
+            
+    
+def load_file_variance(filename, conserved_variables):
+    with netCDF4.Dataset(filename) as f:
+        for attr in f.ncattrs():
+            plot_info.add_additional_plot_parameters(filename.replace("/", "_") + "_" + attr, f.getncattr(attr))
+        
+        resolution = f.variables[conserved_variables[0]].shape[0]
+        
+        mean = np.zeros((resolution, resolution, len(conserved_variables)))
+        m2 = np.zeros((resolution, resolution, len(conserved_variables)))
+        for sample in range(resolution):
+            for variable_index, variable in enumerate(conserved_variables):
+                key = f'sample_{sample}_{variable}'
+                mean[:,:,variable_index] += f.variables[key][:,:,0]
+                m2[:,:,variable_index] += f.variables[key][:,:,0]**2
+                
+        
+        mean /= resolution
+        m2 /= resolution
+        
+        return m2 - mean**2
+        
+           
+    
+def load_file(filename, conserved_variables, statistic_name):
+    if statistic_name == 'single_sample':
+        return load_file_single_sample(filename, conserved_variables)
+    elif statistic_name == 'mean':
+        return load_file_mean(filename, conserved_variables)
+    elif statistic_name == 'variance':
+        return load_file_variance(filename, conserved_variables)
+    else:
+        raise Exception(f"Unknown statistics: {statistic_name}")
     
     
-def plot_convergence(basename, statistic_name, title, conserved_variables = conserved_variables_stochastic,
+def get_time(filename):
+    with netCDF4.Dataset(filename) as f:
+        return f.variables['time'][0]
+    
+    
+def plot_convergence(basename, statistic_name, title, conserved_variables = conserved_variables_default,
                                 resolutions=[64, 128,256,512,1024]):
     
     # We do reference convergence
     reference_resolution = max(resolutions)
-    reference_solution = load_file(basename.format(resolution=resolution),
-                                   conserved_variables)
+    reference_solution = load_file(basename.format(resolution=reference_resolution),
+                                   conserved_variables, statistic_name)
     
     errors = []
     for resolution in resolutions[:-1]:
+        timepoint = get_time(basename.format(resolution=resolution))
+        
+        
         data = load_file(basename.format(resolution=resolution), 
-                         conserved_variables)
+                         conserved_variables, 
+                         statistic_name)
         
         # Plot the solution
         x, y = np.mgrid[0:1:resolution*1j, 0:1:resolution*1j]
@@ -72,11 +127,11 @@ def plot_convergence(basename, statistic_name, title, conserved_variables = cons
             plt.xlabel("$x$")
             plt.ylabel("$y$")
             
-            plt.title(f"{variable}\n{title}")
+            plt.title(f"{variable}\n{title}\n$T={timepoint}$")
             
             plt.colorbar()
             
-            plot_info.savePlot(f"field_plot_{variable}_{title}")
+            plot_info.savePlot(f"field_plot_{variable}_{title}_{timepoint}")
             plt.close('all')
         
         # Upscale
@@ -88,8 +143,8 @@ def plot_convergence(basename, statistic_name, title, conserved_variables = cons
         
         errors.append(error)
         
-    plot_info.saveData(f'convergence_{statistic_name}_{title}_errors', errors)
-    plot_info.saveData(f'convergence_{statistic_name}_{title}_resolutions', resolutions)
+    plot_info.saveData(f'convergence_{statistic_name}_{title}_{timepoint}_errors', errors)
+    plot_info.saveData(f'convergence_{statistic_name}_{title}_{timepoint}_resolutions', resolutions)
     
     plt.loglog(resolutions[:-1], errors, '-o')
     poly = np.polyfit(np.log(resolutions[:-1]), np.log(errors), 1)
@@ -101,7 +156,7 @@ def plot_convergence(basename, statistic_name, title, conserved_variables = cons
     plt.xlabel("Resolution ($N\\times N$)")
     plt.ylabel(f"Error ($||{stats_latex(statistic_name, 'N')}-{stats_latex(statistic_name, str(reference_resolution))}||_{{L^1(D)}}$)")
     
-    plt.title(f'Convergence of {statistic_name}\n{title}')
+    plt.title(f'Convergence of {statistic_name}\n{title}\n$T={timepoint}$')
     
     plot_info.saveData(f'convergence_{statistic_name}_{title}')
     plt.close('all')
@@ -127,12 +182,8 @@ Computes the wasserstein distances
     args = parser.parse_args()
     
     
-    if args.statistics_name == 'single_sample':
-        conserved_variables = conserved_variables_deterministic
-    else:
-        conserved_variables = conserved_variables_stochastic
-        
+
     plot_convergence(args.input_basename, 
                      args.statistic_name, 
                      args.title, 
-                     conserved_variables=conserved_variables)
+                     conserved_variables=conserved_variables_default)
