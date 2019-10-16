5dcbee7cd246fada5e8be1fe5b9c1cfc0244f550
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-10-16 17:59:44 +0200

    small fixes to shockvortex

--- experiments_full_time_average/shockvortex/template/shockvortex.py
+++ experiments_full_time_average/shockvortex/template/shockvortex.py
@@ -21,17 +21,17 @@ else:
     uy = 0.0;
     p = 1 - 0.1 * GAMMA;
 
-
-
+# same value as https://link.springer.com/chapter/10.1007/BFb0096355 page 407
+vortex_epsilon=0.3
 # vortex part
 if x < 0.5:
     tau = sqrt(pow(x - x_c, 2) + pow(y - y_c, 2)) / r_c;
     sin_theta = (y - y_c) / (tau * r_c);
     cos_theta = (x - x_c) / (tau * r_c);
 
-    ux += epsilon * tau * exp(alpha*(1 - pow(tau, 2))) * sin_theta;
-    uy += -epsilon * tau * exp(alpha*(1 - pow(tau, 2))) * cos_theta;
-    p += -(GAMMA - 1) * pow(epsilon, 2) * exp(2 * alpha*(1 - pow(tau, 2))) / (4 * alpha * GAMMA) * rho;
+    ux += vortex_epsilon * tau * exp(alpha*(1 - pow(tau, 2))) * sin_theta;
+    uy += -vortex_epsilon * tau * exp(alpha*(1 - pow(tau, 2))) * cos_theta;
+    p += -(GAMMA - 1) * pow(vortex_epsilon, 2) * exp(2 * alpha*(1 - pow(tau, 2))) / (4 * alpha * GAMMA) * rho;
 
 
 
