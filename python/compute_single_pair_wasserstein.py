36d3df056606624039f7e4d4bab2058cc0b69c43
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-11-15 09:47:16 +0100

    fixes to wasserstein one point

--- python/compute_single_pair_wasserstein.py
+++ python/compute_single_pair_wasserstein.py
@@ -81,34 +81,43 @@ def compute_wasserstein_one_point(file_a, file_b, multi_x, multi_y):
     
     start_x, end_x, start_y, end_y = get_node_x_y_ranges(multi_x, multi_y, resolution)
     
-    data_a = load_plane_line(file_a,
-                             start_x // factor, 
-                             end_x // factor,
-                             start_y // factor,
-                             end_y // factor,
-                             resolution, 
-                             conserved_variables)
-    
-    data_b = load_plane_line(file_b,
-                             start_x, 
-                             end_x,
-                             start_y,
-                             end_y,
-                             resolution, 
-                             conserved_variables)
-    
+    start_y_coarse = y // factor
+    end_y_coarse = max(start_y_coarse + 1, end_y // factor)
+
     wasserstein_distance_sum = 0.0
     weights_a = np.ones(resolution) / resolution
     weights_b = np.ones(resolution) / resolution
+
+
+    for y_coarse in range(start_y_coarse, end_y_coarse):
+        data_a = load_plane_line(file_a,
+                                 start_x // factor, 
+                                 end_x // factor,
+                                 y_coarse,
+                                 y_coarse + 1,
+                                 resolution, 
+                                 conserved_variables)
+
+        for y_fine_offset in range(factor):
+            y = y_coarse * factor + y_fine_offset
+
+            data_b = load_plane_line(file_b,
+                                     start_x, 
+                                     end_x,
+                                     y,
+                                     y + 1,
+                                     resolution, 
+                                     conserved_variables)
     
-    for y in range(end_y - start_y):
-        for x in range(end_x - start_x):
-            distances = ot.dist(data_a[:,y//factor, x//factor,:],
-                                data_b[:,y, x,:], metric='euclidean')
+    
+
+            for x in range(end_x - start_x):
+                distances = ot.dist(data_a[:, y_coarse, x//factor, :],
+                                    data_b[:, y, x, :], metric='euclidean')
                             
-            emd_pairing = ot.emd(weights_a, weights_b, distances)
-            wasserstein_distance = np.sum(emd_pairing * distances)
-            wasserstein_distance_sum += wasserstein_distance
+                emd_pairing = ot.emd(weights_a, weights_b, distances)
+                wasserstein_distance = np.sum(emd_pairing * distances)
+                wasserstein_distance_sum += wasserstein_distance
 
     return wasserstein_distance_sum / resolution**2
     
